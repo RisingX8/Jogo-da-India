a220cfcfd9266fe837bdb7983b62e89f8f9f680a
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2021-07-06 22:22:17 -0300

    Para não dar erro

---
+++ New Unity Project/Assets/Invector-3rdPersonController/Add-ons/Inventory Craft System/Scripts/PartialItemClass/vItemCraftable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Invector.vItemManager
+{
+    /// <summary>
+    /// Used to Craft an item
+    /// </summary>
+    [System.Serializable]
+    public class vItemCraft
+    {
+        public vItem item;
+        public int amount;
+    }
+    public partial class vItem
+    {
+        /// <summary>
+        /// List of materials necessary to craft a new item
+        /// </summary>
+        [HideInInspector]
+        public List<vItemCraft> itemsToCraft = new List<vItemCraft>();
+        [HideInInspector]
+        public bool canCraft = true;
+        [HideInInspector]
+        public int price = 0;
+    }
+}
